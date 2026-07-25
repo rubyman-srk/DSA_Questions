b28213d6c992e1417ee9728ea13263f92521ba7f
Sarthak Kapse <57992055+rubyman-srk@users.noreply.github.com>
2026-07-25 23:40:02 +0530

    Add files via upload

---
+++ p_75.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i=0;
+        int j=nums.size()-1;
+        int k=0;
+        while(k<=j){
+            if(nums[k]==0){
+                swap(nums[k], nums[i]);
+                i++;
+                k++;
+            }
+            else if(nums[k]==2){
+                swap(nums[k], nums[j]);
+                j--;
+                
+            }
+            else if(nums[k]==1){
+                k++;
+            }
+        }
+    }
+};
